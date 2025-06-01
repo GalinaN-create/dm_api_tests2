f56480e1d68c95e744e2421b4b6f9b4e39f2c82c
DESKTOP-JJJ8EAV\Галюшка <galina.nosova.99@mail.ru>
2025-06-02 00:43:17 +0500

    fix helper tests

--- tests/functional/delete_v1_account_login/test_delete_v1_account_login.py
+++ tests/functional/delete_v1_account_login/test_delete_v1_account_login.py
@@ -1,4 +1,4 @@
 def test_delete_v1_account_login(
-        account_helper
+        auth_account_helper
 ):
-    account_helper.dm_api_account.login_api.delete_v1_account_login()
+    auth_account_helper.dm_api_account.login_api.delete_v1_account_login()
