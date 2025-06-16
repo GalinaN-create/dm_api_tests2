3d55b122dbac698980fe4ef6f3a738d180ef9ec6
DESKTOP-JJJ8EAV\Галюшка <galina.nosova.99@mail.ru>
2025-06-16 12:02:48 +0500

    Add context manager in tests

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -9,11 +9,14 @@ from hamcrest import assert_that, \
     instance_of, \
     starts_with
 
+from checkers.http_checkers import check_status_code_http
+
 
 def test_get_v1_account(
         account_helper
 ):
-    account_helper.dm_api_account.account_api.get_v1_account()
+    with check_status_code_http(401, "User must be authenticated"):
+        account_helper.dm_api_account.account_api.get_v1_account()
 
 
 def test_get_v1_account_auth(
