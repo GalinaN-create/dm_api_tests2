343e0850b250d9999915f69ac132d4306127118a
DESKTOP-JJJ8EAV\Галюшка <galina.nosova.99@mail.ru>
2025-06-17 00:48:08 +0500

    Add check_status_code_http in test_put_v1_account_email

--- tests/functional/test_put_v1_account_email.py
+++ tests/functional/test_put_v1_account_email.py
@@ -1,3 +1,6 @@
+from checkers.http_checkers import check_status_code_http
+
+
 def test_put_v1_account_email(
         account_helper,
         prepare_user
@@ -10,8 +13,8 @@ def test_put_v1_account_email(
     account_helper.activate_token(token=token)
     account_helper.login_user(login=login, password=password)
     account_helper.update_email(login=login, password=password, email=email)
-    response = account_helper.login_user(login=login, password=password)
-    assert response.status_code == 403, "Авторизовались без активации токена при смене почты, а не должны"
+    with check_status_code_http(403, "User is inactive. Address the technical support for more details"):
+        account_helper.login_user(login=login, password=password)
     token_2 = account_helper.get_token_by_email(login=login)
     account_helper.activate_token(token=token_2)
     response = account_helper.login_user(login=login, password=password)
